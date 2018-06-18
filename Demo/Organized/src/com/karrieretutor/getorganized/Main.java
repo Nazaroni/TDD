6318decd8b4f0e6047737f5d0937725129bdedb7
Nazar Malyy <nazar.malyy@gmail.com>
2018-06-18 23:35:47 +0200

    update workspace3

--- Demo/Organized/src/com/karrieretutor/getorganized/Main.java
+++ Demo/Organized/src/com/karrieretutor/getorganized/Main.java
@@ -3,6 +3,8 @@ package com.karrieretutor.getorganized;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("Wasap, let's get organized!!!");
+
     }
 }
